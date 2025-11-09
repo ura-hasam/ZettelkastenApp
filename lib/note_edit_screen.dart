da56d8d56f7e79237cf84034d0171d66885cbefb
s.yamane0601 <s.yamane0601@gmail.com>
2025-11-09 16:31:02 +0000

    ノートを作成する画面を実装

---
+++ lib/note_edit_screen.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+
+class NoteEditScreen extends StatefulWidget {
+  const NoteEditScreen({super.key});
+
+  @override
+  _NoteEditScreenState createState() => _NoteEditScreenState();
+}
+
+class _NoteEditScreenState extends State<NoteEditScreen> {
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Note'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () {
+              // TODO: Save note
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(
+                labelText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: TextField(
+                controller: _contentController,
+                decoration: const InputDecoration(
+                  labelText: 'Content',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+                expands: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
