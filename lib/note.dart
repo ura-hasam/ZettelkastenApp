c819e966eb35f403a24c2e675fa5c257a5ec9356
s.yamane0601 <s.yamane0601@gmail.com>
2025-11-09 16:26:16 +0000

    データモデルの定義

---
+++ lib/note.dart
@@ -0,0 +1,14 @@
+
+class Note {
+  final String id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+}
